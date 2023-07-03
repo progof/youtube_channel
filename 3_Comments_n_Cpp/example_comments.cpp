ff68bc4a8ab9b9887c18630ce0982dc02f52a0a0
progof <34421475+progof@users.noreply.github.com>
2023-07-03 11:39:15 +0200

    video #3

---
+++ 3_Comments_n_Cpp/example_comments.cpp
@@ -0,0 +1,17 @@
+//Single Line Comments
+
+
+// declaring a variable
+int a;
+
+// initializing the variable 'a' with the value 2
+a = 2;
+
+// ------------------------------------------------
+
+// Multi-line comments
+
+/* declaring a variable
+to store salary to employees
+*/
+int salary = 2000;
